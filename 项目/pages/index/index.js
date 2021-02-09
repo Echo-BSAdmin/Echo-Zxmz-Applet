55b76fbf12c47ad31a9525d5fc1d78f5786df1e3
GerryZhao <15991808959@163.com>
2021-02-09 10:24:38 +0800

    index页面

--- 项目/pages/index/index.js
+++ 项目/pages/index/index.js
@@ -6,51 +6,99 @@ Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    motto: 'Hi 开发者！',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    isShowAnimationContainer: true,
+    dataLists: [],
+    elements: [{
+        title: '布局',
+        name: 'layout',
+        color: 'cyan',
+        icon: 'newsfill'
+      },
+      {
+        title: '背景',
+        name: 'background',
+        color: 'blue',
+        icon: 'colorlens'
+      },
+      {
+        title: '文本',
+        name: 'text',
+        color: 'purple',
+        icon: 'font'
+      },
+      {
+        title: '图标 ',
+        name: 'icon',
+        color: 'mauve',
+        icon: 'icon'
+      },
+      {
+        title: '按钮',
+        name: 'button',
+        color: 'pink',
+        icon: 'btn'
+      },
+      {
+        title: '标签',
+        name: 'tag',
+        color: 'brown',
+        icon: 'tagfill'
+      },
+      {
+        title: '头像',
+        name: 'avatar',
+        color: 'red',
+        icon: 'myfill'
+      },
+      {
+        title: '进度条',
+        name: 'progress',
+        color: 'orange',
+        icon: 'icloading'
+      },
+      {
+        title: '边框阴影',
+        name: 'shadow',
+        color: 'olive',
+        icon: 'copy'
+      },
+      {
+        title: '加载',
+        name: 'loading',
+        color: 'green',
+        icon: 'loading2'
+      },
+    ],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  nextpage(event) {
+    var anmiaton = e.currentTarget.dataset.class;
+    var that = this;
+    that.setData({
+      animation: anmiaton
     })
+    setTimeout(function () {
+      that.setData({
+        animation: ''
+      })
+    }, 1000)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  handletouchmove: function (A) {
+    A.touches[0].pageX;
+    this.opacityAnimate();
+  },
+  opacityAnimate: function (event) {
+    let {
+      animation
+    } = this.data;
+    animation = wx.createAnimation({
+      duration: 1500,
+      timingFunction: "ease-in-out",
+      delay: 0,
+      transformOrigin: "50% 50% 0%"
+    })
+    animation.translate(0, -1300).step(),
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+        animationData: animation.export()
+      });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
