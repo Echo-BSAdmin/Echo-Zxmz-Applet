604309d61ccd5b1ec28db4d93275d2ed63836beb
GerryZhao <15991808959@163.com>
2021-02-10 21:19:57 +0800

    添加index页面动画效果

--- 项目/pages/index/index.js
+++ 项目/pages/index/index.js
@@ -1,104 +1,66 @@
-//index.js
-//获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
+    animation: null,
     isShowAnimationContainer: true,
-    dataLists: [],
     elements: [{
-        title: '布局',
-        name: 'layout',
+        title: '个人资料',
+        name: 'profile',
         color: 'cyan',
-        icon: 'newsfill'
+        icon: 'profile'
       },
       {
-        title: '背景',
-        name: 'background',
-        color: 'blue',
-        icon: 'colorlens'
-      },
-      {
-        title: '文本',
-        name: 'text',
+        title: '工作经历',
+        name: 'experience',
         color: 'purple',
-        icon: 'font'
+        icon: 'addressbook'
       },
       {
-        title: '图标 ',
-        name: 'icon',
+        title: '项目经验',
+        name: 'project',
         color: 'mauve',
-        icon: 'icon'
+        icon: 'qr_code'
       },
       {
-        title: '按钮',
-        name: 'button',
+        title: '职业技能',
+        name: 'skill',
         color: 'pink',
-        icon: 'btn'
-      },
-      {
-        title: '标签',
-        name: 'tag',
-        color: 'brown',
-        icon: 'tagfill'
-      },
-      {
-        title: '头像',
-        name: 'avatar',
-        color: 'red',
-        icon: 'myfill'
-      },
-      {
-        title: '进度条',
-        name: 'progress',
-        color: 'orange',
-        icon: 'icloading'
-      },
-      {
-        title: '边框阴影',
-        name: 'shadow',
-        color: 'olive',
-        icon: 'copy'
-      },
-      {
-        title: '加载',
-        name: 'loading',
-        color: 'green',
-        icon: 'loading2'
-      },
+        icon: 'selectionfill'
+      }
     ],
   },
-  nextpage(event) {
-    var anmiaton = e.currentTarget.dataset.class;
-    var that = this;
-    that.setData({
-      animation: anmiaton
-    })
-    setTimeout(function () {
-      that.setData({
-        animation: ''
-      })
-    }, 1000)
+  handleTouchMove(event) {
+    var self = this;
+    event.touches[0].pageX;
+    self.opacityAnimate();
+    self.showIndexContainer();
   },
-  handletouchmove: function (A) {
-    A.touches[0].pageX;
-    this.opacityAnimate();
-  },
-  opacityAnimate: function (event) {
+  opacityAnimate(event) {
+    var self = this;
     let {
       animation
-    } = this.data;
+    } = self.data;
     animation = wx.createAnimation({
       duration: 1500,
       timingFunction: "ease-in-out",
       delay: 0,
       transformOrigin: "50% 50% 0%"
-    })
-    animation.translate(0, -1300).step(),
-      this.setData({
-        animationData: animation.export()
-      });
+    });
+    animation.translate(0, -1300).step();
+    self.setData({
+      animationData: animation.export()
+    });
   },
+  showIndexContainer() {
+    var self = this;
+    setTimeout(function () {
+      self.setData({
+        isShowAnimationContainer: false,
+        toggleDelay: true
+      });
+     }, 1000);
+  }
 })
